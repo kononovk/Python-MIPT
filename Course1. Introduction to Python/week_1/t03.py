961247f3ccdd2d6ccce1c0277d0011350db09b2b
Nickolay <kononov.nikolay.nk1@gmail.com>
2019-10-11 11:32:33 +0300

    commition1

---
+++ Course1. Introduction to Python/week_1/t03.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+import sys
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+
+d = b**2 - 4*a*c
+if a == 0 and b == 0 and c == 0:
+    print("all x are roots")
+elif a == 0 and b == 0:
+    print("no roots")
+elif a == 0:
+    print(int(-c / b))
+else:
+    d = b**2 - 4*a*c
+    if d < 0:
+        print("no roots")
+    elif d == 0:
+        print(int(-b/2*a))
+    else:
+        print(int((-b + sqrt(d))/2*a))
+        print(int((-b - sqrt(d))/2*a))
