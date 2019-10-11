af5a078fb36e88c556af5b1950a3cfa80c8f4186
Nickolay <kononov.nikolay.nk1@gmail.com>
2019-10-11 15:54:52 +0300

    New tasks

---
+++ Course1. Introduction to Python/week_2/t02_json_decorator.py
@@ -0,0 +1,28 @@
+import functools
+import json
+
+def to_json(func):
+    @functools.wraps(func)
+    def wraped(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return json.dumps(result)
+    return wraped
+
+@to_json
+def get_data1():
+    return { 'data': 42, 'data2' : 3, 'data3' : 42 }
+
+@to_json
+def get_data2():
+    return [1, 2, 3]
+
+@to_json
+def get_data3(a, b):
+    return (a, b)
+
+if __name__ == '__main__':
+    print(get_data1())
+    print(get_data2())
+    print(get_data3(1, 2))
+
+# get_data -> to_json(get_data())
